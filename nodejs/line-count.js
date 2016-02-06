f2ee10efa91a7ff8558d56a0697dab72860e8b65
Praveen <prawin20@gmail.com>
2016-02-06 21:46:08 +0530

    Create line-count.js
    number of line in file. File as argument

---
+++ nodejs/line-count.js
@@ -0,0 +1,18 @@
+
+var file = require("fs");
+var buffer = file.readFileSync(process.argv[2]);
+var lineCount = buffer.toString().split("\n").length-1;
+console.log(lineCount);
+
+
+
+ /*var fs = require('fs');  
+       
+     var contents = fs.readFileSync(process.argv[2]);  
+     var lines = contents.toString().split('\n').length - 1;  
+     console.log(lines) ; 
+       
+     // note you can avoid the .toString() by passing 'utf8' as the  
+     // second argument to readFileSync, then you'll get a String!  
+     //  
+     // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1  */
