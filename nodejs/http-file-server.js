9096b9fbd1f52bb63c15d9ad42f2ab46c2ca4e4e
Praveen <prawin20@gmail.com>
2016-02-11 10:43:03 +0530

    Create http-file-server.js
    Https server return file data. First command line argument is port number and second is file path.

---
+++ nodejs/http-file-server.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var fs = require("fs");
+var server = http.createServer(function(request,response){
+var data = fs.createReadStream(process.argv[3]).pipe(response);  
+});
+server.listen(process.argv[2]);
+
+/*
+ var http = require('http')  
+     var fs = require('fs')  
+       
+     var server = http.createServer(function (req, res) {  
+       res.writeHead(200, { 'content-type': 'text/plain' })  
+       
+       fs.createReadStream(process.argv[3]).pipe(res)  
+     })  
+       
+     server.listen(Number(process.argv[2]))  
+*/
