aab631f1328a109358cbbbc6a024e2bd5f038e7e
Praveen <prawin20@gmail.com>
2016-02-11 15:38:32 +0530

    Create hello-world.js
    Hello world for express module in node,js

---
+++ nodejs/express/hello-world.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+app.get("/home",function(request,response){
+    response.end("Hello World!");
+    });
+    app.listen(process.argv[2]);
+    
+/* var express = require('express')
+    var app = express()
+    app.get('/home', function(req, res) {
+      res.end('Hello World!')
+    })
+    app.listen(process.argv[2])*/    
+    
