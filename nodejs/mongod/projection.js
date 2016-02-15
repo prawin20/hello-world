9ae60d0860d5d44d0000c520710fc8949a72bbc2
Praveen <prawin20@gmail.com>
2016-02-15 11:22:23 +0530

    Create projection.js
    Projecting only particular fields in the document.

---
+++ nodejs/mongod/projection.js
@@ -0,0 +1,39 @@
+var mongo = require('mongodb').MongoClient;
+ var url = "mongodb://localhost:27017/learnyoumongo";
+    mongo.connect(url, function(err, db) {
+        if(err){
+            console.log(err);
+        }
+       db.collection('parrots').find({"age":{$gt:parseInt(process.argv[2])}},{name:1,age:1,_id:0}).toArray(function(err,documents){
+            if(err){
+            console.log(err);
+        }
+           console.log(documents);
+           db.close();
+       });
+    });
+    
+/*
+   var mongo = require('mongodb').MongoClient
+    var age = process.argv[2]
+    
+    var url = 'mongodb://localhost:27017/learnyoumongo'
+    
+    mongo.connect(url, function(err, db) {
+      if (err) throw err
+      var parrots = db.collection('parrots')
+      parrots.find({
+        age: {
+          $gt: +age
+        }
+      }, {
+        name: 1
+      , age: 1
+      , _id: 0
+      }).toArray(function(err, docs) {
+        if (err) throw err
+        console.log(docs)
+        db.close()
+      })
+    })
+*/
